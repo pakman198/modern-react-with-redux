a3cdf9d68a2176d3296d83ed661b9f1d1c0a622f
Pako Herrera <pakman_198@hotmail.com>
2017-12-06 17:36:16 -0600

    Added the 4 projects created on the course

---
+++ Redux Bookshelf/src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function(){
+  return [
+    {title: 'Javascript: The Good Parts'},
+    {title: 'The Hobbit'},
+    {title: 'Game of Thrones: A Song of Ice and Fire'},
+    {title: 'The Lord of the Rings: The Fellowship of the Ring'},
+  ]
+}
