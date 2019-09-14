a25cb590baafc991bb925a4c6c74dc985e7dede4
Pako Herrera <pakman_198@hotmail.com>
2019-09-14 14:10:11 -0700

    Started from 0 for the 2019 course update

--- Redux Bookshelf/src/reducers/reducer_books.js
+++
@@ -1,8 +0,0 @@
-export default function(){
-  return [
-    {title: 'Javascript: The Good Parts'},
-    {title: 'The Hobbit'},
-    {title: 'Game of Thrones: A Song of Ice and Fire'},
-    {title: 'The Lord of the Rings: The Fellowship of the Ring'},
-  ]
-}
--- Weather App/src/reducers/index.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from 'redux';
-import WeatherReducer from './weather';
-
-const rootReducer = combineReducers({
-  weather: WeatherReducer
-});
-
-export default rootReducer;
