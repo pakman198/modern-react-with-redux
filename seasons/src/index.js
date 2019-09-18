f944335abc82f915f1606df8053bd041f47b7f04
pako herrera <pakman_198@hotmail.com>
2019-09-18 15:49:12 -0700

    Conditional rendering

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -3,13 +3,36 @@ import ReactDOM from 'react-dom';
 
 class App extends React.Component {
   
-  render() {
-    window.navigator.geolocation.getCurrentPosition(
-      (position) => console.log(position),
-      (err) => console.log(err)
-    )
+  constructor(props){
+    super(props)
 
-    return <div>Hello World</div>
+    this.state = {
+      lat: null,
+      errorMessage: ''
+    }
+
+    window.navigator.geolocation.getCurrentPosition(
+      (position) => {
+        console.log(position)
+        this.setState({lat: position.coords.latitude})
+      },
+      (err) => {
+        this.setState({ errorMessage: err.message })
+      }
+    )
+  }
+
+  render() {
+    const { errorMessage, lat } = this.state;
+
+    if (errorMessage && !lat) {
+      return <div>Error: {errorMessage}</div>
+
+    } else if (!errorMessage && lat) {
+      return <div>Latitude: {lat}</div>
+    }
+
+    return <div>Loading...</div>
   }
 }
 
