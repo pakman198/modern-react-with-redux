0a9e4184d63911686a3b097f328acd2f494b955d
pako herrera <pakman_198@hotmail.com>
2019-09-18 14:21:35 -0700

    Created 2 functional components, learned the importance of jsx, props and reusability

--- Comments App/src/index.js
+++ Comments App/src/index.js
@@ -1,4 +1,52 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import faker from 'faker';
 
-ReactDOM.render(<h1>Hello world</h1>, document.getElementById('root'));
+import ApprovalCard from './ApprovalCard';
+import CommentDetail from './CommentDetail';
+
+const App = () => {
+	return (
+		<div className="ui three column grid">
+  		<div className="four column row">
+				<div className="column">
+					<div className="ui container comments">
+					<ApprovalCard>
+							<h4>Warning!</h4>
+							<p>Are you sure?</p>
+						</ApprovalCard>
+
+						<ApprovalCard>
+							<CommentDetail 
+								author="Sam"
+								timeAgo="Today at 4:45PM"
+								content="Nice blog post"
+								avatar={faker.image.avatar()}
+							/>
+						</ApprovalCard>
+
+						<ApprovalCard>
+							<CommentDetail 
+								author="Alex"
+								timeAgo="Today at 2:00AM"
+								content="Amazing job!"
+								avatar={faker.image.avatar()}
+							/>
+						</ApprovalCard>
+
+						<ApprovalCard>
+							<CommentDetail 
+								author="Jane"
+								timeAgo="Yesterday at 6:30PM"
+								content="Thanks for sharing!"
+								avatar={faker.image.avatar()}
+							/>
+						</ApprovalCard>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
