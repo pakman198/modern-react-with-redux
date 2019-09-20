4d80a3ea55dca2370abb4ec2f099154f90c8f0b0
pako herrera <pakman_198@hotmail.com>
2019-09-20 14:54:58 -0700

    Added styling to the image card and used css-grid to display a grid and react refs to add extra css via js to create a masonry-like grid

---
+++ pics/src/components/ImageList.js
@@ -0,0 +1,20 @@
+import './ImageList.css';
+import React from 'react';
+
+import ImageCard from './ImageCard';
+
+const ImageList = (props) => {
+  const images = props.images.map( image => {
+    const { id } = image;
+
+    return <ImageCard key={id} image={image} />
+  });
+
+  return (
+    <div className="image-list">
+      { images }
+    </div>
+  );
+}
+
+export default ImageList;
