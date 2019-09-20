1cf43805595997260d652817eaa157dd26e491be
pako herrera <pakman_198@hotmail.com>
2019-09-20 11:18:13 -0700

    connected both components via props and event handlers

--- pics/src/components/SearchBar.js
+++ pics/src/components/SearchBar.js
@@ -1,13 +1,29 @@
 import React, { Component } from 'react';
 
 class SearchBar extends Component {
+
+  state = {
+    term: ''
+  }
+
+  onFormSubmit = (e) => {
+    e.preventDefault();
+
+    const { term } = this.state;
+
+    this.props.onSubmit(term);
+  }
+
   render() {
     return (
       <div className="ui segment">
-        <form className="ui form">
+        <form className="ui form" onSubmit={ this.onFormSubmit }>
           <div className="field">
             <label>Image search</label>
-            <input type="text"/>
+            <input 
+              type="text" 
+              value={ this.state.term} 
+              onChange={ e => this.setState({ term: e.target.value })} />
           </div>
         </form>
       </div>
