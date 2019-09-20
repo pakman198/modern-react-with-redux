7fccbedd4634c44d50dfc086c9d7500a513c251d
pako herrera <pakman_198@hotmail.com>
2019-09-20 11:44:19 -0700

    Added unsplash API to fetch random images. Added axios to be able to make http requests

--- pics/src/components/App.js
+++ pics/src/components/App.js
@@ -5,8 +5,15 @@ import SearchBar from './SearchBar';
 
 class App extends React.Component {
 
-  onSearchSubmit(term) {
-
+  async onSearchSubmit(term) {
+    const response = await axios.get('https://api.unsplash.com//search/photos', {
+      params: {
+        query: term 
+      },
+      headers: {
+        Authorization: 'Client-ID ed278a199ad7de1f764618b22c840f8d8f854882728eccc910e97c28def89d50'
+      }
+    });
   }
 
   render() {
