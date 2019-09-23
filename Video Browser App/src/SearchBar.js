bb8b29aae84809646336d33f04fc97214d77f17b
pako herrera <pakman_198@hotmail.com>
2019-09-23 10:28:20 -0700

    Video browser app setup

---
+++ Video Browser App/src/SearchBar.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react';
+
+class SearchBar extends Component {
+
+  state = {
+    term: ''
+  }
+
+  onFormSubmit = (e) => {
+    e.preventDefault();
+
+    const { term } = this.state;
+
+    this.props.onSubmit(term);
+  }
+
+  onInputChange = (e) => {
+    const { value } = e.target;
+    this.setState({ term: value })
+  }
+
+  render() {
+    return (
+      <div className="ui segment">
+        <form className="ui form" onSubmit={ this.onFormSubmit }>
+          <div className="field">
+            <label>Image search</label>
+            <input 
+              type="text" 
+              value={ this.state.term  } 
+              onChange={ this.onInputChange } />
+          </div>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default SearchBar;
