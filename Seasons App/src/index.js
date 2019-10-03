5c937b7a32e4f1f3a41ed73162e8e13c397bba98
pako herrera <pakman_198@hotmail.com>
2019-10-03 10:21:48 -0700

    Modified the seasons app to use React Hooks

--- Seasons App/src/index.js
+++ Seasons App/src/index.js
@@ -1,10 +1,35 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import ReactDOM from 'react-dom';
 
 import SeasonDisplay from './SeasonDisplay';
 import Spinner from './Spinner';
 
-class App extends React.Component {
+const App = () => {
+  const [ lat, setLat ] = useState(null);
+  const [ errorMessage, setErrorMessage ] = useState('')
+  
+  useEffect(() => {
+    window.navigator.geolocation.getCurrentPosition(
+      position => setLat(position.coords.latitude),
+      err => setErrorMessage(err.message)
+    )
+  }, []);
+
+  let content;
+  if(errorMessage) {
+    content = <div>Error: {errorMessage}</div>
+  } else if(lat ) {
+    content = <SeasonDisplay lat={lat} />
+  } else {
+    content = <Spinner />
+  }
+
+  return (
+    <div className="border red">{ content }</div>
+  );
+}
+
+/* class _App extends React.Component {
   
   state = {
     lat: null,
@@ -42,7 +67,7 @@ class App extends React.Component {
       </div>
     );
   }
-}
+} */
 
 
 
